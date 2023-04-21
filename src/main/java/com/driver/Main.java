b7927aaa25ce243fe0b7afa8ab259d71d0203462
sanjsngh <sanjivanisngh@gmail.com>
2023-04-21 14:41:29 +0530

    Added getter and setter

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]) {
+      RWOnly r = new RWOnly();
+
+      r.setName("Jake");
+      System.out.println(r.getName());
+      // java: name has private access in com.driver.RWOnly
+      // System.out.println(r.name);
+
+
+  }
 }
